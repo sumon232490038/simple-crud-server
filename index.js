cf9a92b268a556d1d8ff7bb312baaba60ec55cde
sumon232 <sumonmia4526@gmail.com>
2024-12-11 18:46:01 +0600

    alhamdulilah upadating to client site to server site and server site to mongodb is done

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cors());
+app.use(express.json());
 
 const uri =
   "mongodb+srv://sumonmia4526:ry7cI7qFcAijgVKk@cluster0.xhl2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -20,40 +21,60 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollections = client.db("usersDB").collection("userCollections");
-
-    app.get("/users", async (req, res) => {
-      const data = userCollections.find();
-      const result = await data.toArray();
+    app.get("/usersData", async (req, res) => {
+      const cursor = userCollections.find();
+      const result = await cursor.toArray();
       res.send(result);
     });
 
+    app.get("/usersData/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollections.insertOne(user);
+      res.send(result);
+      // console.log();
+    });
 
+    app.put("/usersData/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      // console.log(user);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateUser = {
+        $set: {
+          name: user.name,
+          email: user.email,
+        },
+      };
+
+      const result = await userCollections.updateOne(
+        filter,
+
+        updateUser,
+        options
+      );
       res.send(result);
     });
 
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/usersData/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollections.deleteOne(query);
       res.send(result);
     });
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Server Site is Running parfectly");
   } finally {
   }
 }
-run().catch(console.dir);
+run().catch(console.log);
 
-app.get("/users", (req, res) => {
-  res.json(users);
-});
-
-const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
